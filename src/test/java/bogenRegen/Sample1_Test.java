87a8188cf4d0669bcc22335508204ed16e665df6
MSDilliwal <marutisdilliwal>
2022-07-25 23:00:11 +0530

    v

---
+++ src/test/java/bogenRegen/Sample1_Test.java
@@ -0,0 +1,14 @@
+package bogenRegen;
+
+public class Sample1_Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("work hard to get job...!");
+
+      System.out.println("1");
+      System.out.println("2");
+    //  System.out.println("3");
+
+	}
+}
